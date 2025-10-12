ddffe7c44861bf9576a30956a38c7954af829536
Aznatdor <abobrov333@gmail.com>
2025-10-12 19:32:07 +0300

    Add support for numbering groups of alterations

--- app/Grep/Parser.hs
+++ app/Grep/Parser.hs
@@ -4,25 +4,26 @@ import Grep.Types
 
 -- Given pattern string, converts it into list of tokens
 -- Token types are listed in app/Grep/Types.hs
-parsePattern :: String -> [PatternToken]
-parsePattern [] = []
-parsePattern ('\\':'d':rest) = Meta Digit : parsePattern rest
-parsePattern ('\\':'w':rest) = Meta Word  : parsePattern rest
-parsePattern ('[':'^':xs)  = let (group, rest) = span (/=']') xs in
-                                Group (map Literal group) True : parsePattern (drop 1 rest)
-parsePattern ('[':xs) = let (group, rest) = span (/=']') xs in
-                            Group (map Literal group) False : parsePattern (drop 1 rest)
-parsePattern (')':xs) = error $ "unexpected ')'"
-parsePattern ('(':xs) =
-    let (alterationGroup, rest) = parseAlteration xs [[]]
-    in Alteration alterationGroup : parsePattern rest
-parsePattern ('^':rest) = AnchorStart : parsePattern rest
-parsePattern ('$':rest) = AnchorEnd : parsePattern rest -- rest is []!
-parsePattern ('+':rest) = Plus : parsePattern rest
-parsePattern ('*':rest) = Star : parsePattern rest
-parsePattern ('?':rest) = Question : parsePattern rest
-parsePattern ('.':rest) = Wildcard : parsePattern rest
-parsePattern (char:rest) = Literal char : parsePattern rest
+parsePattern :: String -> Int -> [PatternToken]
+parsePattern [] _ = []
+parsePattern ('\\':'d':rest) groupNum = Meta Digit : (parsePattern rest groupNum)
+parsePattern ('\\':'w':rest) groupNum = Meta Word  : (parsePattern rest groupNum)
+parsePattern ('[':'^':xs)    groupNum =
+    let (group, rest) = span (/=']') xs
+    in Group (map Literal group) True : (parsePattern (drop 1 rest) groupNum)
+parsePattern ('[':xs) groupNum = let (group, rest) = span (/=']') xs in
+                            Group (map Literal group) False : (parsePattern (drop 1 rest) groupNum)
+parsePattern (')':xs) _ = error $ "unexpected ')'"
+parsePattern ('(':xs) groupNum =
+    let (alterationGroup, rest, newGroupNum) = parseAlteration xs [[]] (groupNum+1) -- newGroupNum should be at lest groupNum+1. Indeed!
+    in Alteration groupNum alterationGroup : (parsePattern rest newGroupNum) -- group is reversed, which should not affect overall result.
+parsePattern ('^':rest) groupNum = AnchorStart : (parsePattern rest groupNum)
+parsePattern ('$':rest) groupNum = AnchorEnd : (parsePattern rest groupNum) -- rest is []!
+parsePattern ('+':rest) groupNum = Plus : (parsePattern rest groupNum)
+parsePattern ('*':rest) groupNum = Star : (parsePattern rest groupNum)
+parsePattern ('?':rest) groupNum = Question : (parsePattern rest groupNum)
+parsePattern ('.':rest) groupNum = Wildcard : (parsePattern rest groupNum)
+parsePattern (char:rest) groupNum = Literal char : (parsePattern rest groupNum)
 
 -- Joins repeater token and token to be repeated
 applyRepeat :: [PatternToken] -> [PatternToken]
@@ -34,28 +35,29 @@ applyRepeat (token:rest) = token : applyRepeat rest
 
 
 -- Helper function to parse group of alteration, e.g. (ab|c|de)
-parseAlteration :: String -> [[PatternToken]]-> ([[PatternToken]], String)
+-- returns content of group, rest of the string to parse, next group number
+parseAlteration :: String -> [[PatternToken]] -> Int -> ([[PatternToken]], String, Int)
 -- End of the group. Return the result
-parseAlteration (')':rest) acc = (acc, rest)
+parseAlteration (')':rest) acc groupNum = (acc, rest, groupNum)
 -- Inner group of alterations
 -- For this case we should also know what is going after the group
-parseAlteration ('(':rest) acc = parseAlteration after updatedAcc
-    where (inner, after) = parseAlteration rest [[]]
-          innerAlteration = Alteration inner
+parseAlteration ('(':rest) acc groupNum = parseAlteration after updatedAcc newGroupNum
+    where (inner, after, newGroupNum) = parseAlteration rest [[]] (groupNum+1)
+          innerAlteration = Alteration groupNum inner
           newHead = (head acc ++ [innerAlteration])
           updatedAcc = newHead : (drop 1 acc)
 -- Add new alteration
-parseAlteration ('|':rest) acc = parseAlteration rest ([] : acc)
+parseAlteration ('|':rest) acc groupNum = parseAlteration rest ([] : acc) groupNum
 -- Regular case: just append current token to the first list
-parseAlteration ('\\':'w':rest) acc = parseAlteration rest updatedAcc
+parseAlteration ('\\':'w':rest) acc groupNum = parseAlteration rest updatedAcc groupNum
     where token = Meta Word
           newHead = (head acc ++ [token]) -- slower than ':'. But without reversal
           updatedAcc = newHead : (drop 1 acc) -- tail won't work with list of length 1
-parseAlteration ('\\':'d':rest) acc = parseAlteration rest updatedAcc
+parseAlteration ('\\':'d':rest) acc groupNum = parseAlteration rest updatedAcc groupNum
     where token = Meta Digit
           newHead = (head acc ++ [token])
           updatedAcc = newHead : (drop 1 acc)
-parseAlteration (c:rest) acc = parseAlteration rest updatedAcc
+parseAlteration (c:rest) acc groupNum = parseAlteration rest updatedAcc groupNum
     where token = Literal c
           newHead = (head acc) ++ [token]
           updatedAcc = newHead : (drop 1 acc)
--- app/Grep/Types.hs
+++ app/Grep/Types.hs
@@ -1,6 +1,7 @@
 module Grep.Types where
 
 type Negative = Bool
+type GroupNum = Int
 
 data PatternToken = Literal Char
     | Meta MetaType 
@@ -10,6 +11,7 @@ data PatternToken = Literal Char
     | Star | Plus | Question                -- Option types for  preproccessing
     | Repeater PatternToken Int (Maybe Int) -- for matching
     | Wildcard
+    | Alteration GroupNum [[PatternToken]]
     deriving (Show, Eq)
 
 data MetaType = Word | Digit deriving (Show, Eq)
