6ab65165429c93d28a1e002f6ce08e95d2835f6e
Aznatdor <abobrov333@gmail.com>
2025-10-12 19:09:01 +0300

    Fixed bug in case of inner group of alterations

--- app/Grep/Parser.hs
+++ app/Grep/Parser.hs
@@ -12,6 +12,10 @@ parsePattern ('[':'^':xs)  = let (group, rest) = span (/=']') xs in
                                 Group (map Literal group) True : parsePattern (drop 1 rest)
 parsePattern ('[':xs) = let (group, rest) = span (/=']') xs in
                             Group (map Literal group) False : parsePattern (drop 1 rest)
+parsePattern (')':xs) = error $ "unexpected ')'"
+parsePattern ('(':xs) =
+    let (alterationGroup, rest) = parseAlteration xs [[]]
+    in Alteration alterationGroup : parsePattern rest
 parsePattern ('^':rest) = AnchorStart : parsePattern rest
 parsePattern ('$':rest) = AnchorEnd : parsePattern rest -- rest is []!
 parsePattern ('+':rest) = Plus : parsePattern rest
@@ -29,55 +33,29 @@ applyRepeat (token:Question:rest) = Repeater token 0 (Just 1) : applyRepeat rest
 applyRepeat (token:rest) = token : applyRepeat rest
 
 
--- Function to parse group of alterations (..)
--- parseAlteration :: Int -> String -> ([PatternToken], String)
--- parseAlteration groupNum (')':rest) = ([], rest) -- end of the group
--- -- Group inside the current group
--- parseAlteration groupNum ('(':rest) =
---     let 
--- parseAlteration groupNum ('\\':'d':rest) = 
---     let (group, after) = parseAlteration groupNum rest
---     in (Meta Digit:group, after)
--- parseAlteration groupNum ('\\':'w':rest) = 
---     let (group, after) = parseAlteration groupNum rest
---     in (Meta Word:group, after)
--- parseAlteration groupNum (c:rest) =
---     let (group, after) = parseAlteration groupNum rest
---     in (Literal cd:group, after)
-
-
+-- Helper function to parse group of alteration, e.g. (ab|c|de)
 parseAlteration :: String -> [[PatternToken]]-> ([[PatternToken]], String)
 -- End of the group. Return the result
 parseAlteration (')':rest) acc = (acc, rest)
 -- Inner group of alterations
 -- For this case we should also know what is going after the group
-parseAlteration ('(':rest) acc =
-    let (newAcc, rest1) = parseAlteration after updatedAcc
-    in (newAcc, rest1)
+parseAlteration ('(':rest) acc = parseAlteration after updatedAcc
     where (inner, after) = parseAlteration rest [[]]
           innerAlteration = Alteration inner
           newHead = (head acc ++ [innerAlteration])
-          updatedAcc = newHead : acc
+          updatedAcc = newHead : (drop 1 acc)
 -- Add new alteration
-parseAlteration ('|':rest) acc =
-    let (newAcc, after) = parseAlteration rest ([] : acc)
-    in (newAcc, after)
+parseAlteration ('|':rest) acc = parseAlteration rest ([] : acc)
 -- Regular case: just append current token to the first list
-parseAlteration ('\\':'w':rest) acc =
-    let (newAcc, after) = parseAlteration rest updatedAcc
-    in (newAcc, after)
+parseAlteration ('\\':'w':rest) acc = parseAlteration rest updatedAcc
     where token = Meta Word
           newHead = (head acc ++ [token]) -- slower than ':'. But without reversal
           updatedAcc = newHead : (drop 1 acc) -- tail won't work with list of length 1
-parseAlteration ('\\':'d':rest) acc =
-    let (newAcc, after) = parseAlteration rest updatedAcc
-    in (newAcc, after)
+parseAlteration ('\\':'d':rest) acc = parseAlteration rest updatedAcc
     where token = Meta Digit
           newHead = (head acc ++ [token])
           updatedAcc = newHead : (drop 1 acc)
-parseAlteration (c:rest) acc = 
-    let (newAcc, after) = parseAlteration rest updatedAcc
-    in (newAcc, after)
+parseAlteration (c:rest) acc = parseAlteration rest updatedAcc
     where token = Literal c
-          newHead = (head acc ++ [token])
+          newHead = (head acc) ++ [token]
           updatedAcc = newHead : (drop 1 acc)
