564279cc945089424ef8ba8e7e482c84b5561bb6
Aznatdor <abobrov333@gmail.com>
2025-10-14 18:53:48 +0300

    Add "Extra" functions to handle backreferences
    The main idea is to consider a collection of environments and
    corresponding inputs.
    Each environment (here it is associative list) map number of group
    to whatever this group has happened to match earlier.
    
    This way instead of checking current input's character, we
    should check every possible branch and filter out what isn't
    matching current pattern.

--- app/Grep/Matcher.hs
+++ app/Grep/Matcher.hs
@@ -1,7 +1,7 @@
-module Grep.Matcher (mainMatch) where
+module Grep.Matcher (mainMatch, matchPatternExtra) where
 
-import Data.Char (isDigit, isLetter)
-import Data.List (findIndex, tails)
+import Data.Char (isDigit)
+import Data.List (tails, isPrefixOf)
 import Grep.Types
 import Grep.Parser
 
@@ -21,6 +21,7 @@ mainMatch rawPattern input
 
 -- matches repeated token and returns all possible input tails after matching
 -- For example: repeatMatches "a" 1 3 "aaaab" -> ["aaab", "aab", "ab"]
+-- TO-DO: add support for alterations, backreferences
 repeatMatches :: PatternToken -> Int -> Maybe Int -> String -> [String]
 repeatMatches _ _ _ [] = [[]]
 repeatMatches token minRep Nothing input
@@ -56,27 +57,90 @@ matchPattern (Group group neg:rest) (c:input)
 matchPattern (Literal x:rest) (c:input)
     | (x == c) = matchPattern rest input
     | otherwise = False  
-matchPattern (Repeater token minRep maxRep:rest) input = 
-    any (matchPattern rest) $ repeatMatches token minRep maxRep input
 matchPattern (Alteration _ alts:rest) input =
     any (True==) [matchPattern (alt ++ rest) input | alt <- alts] 
 
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+-- Code to deal with patterns with backreferences
+--------------------------------------------------------------------------------------------------------------------------------------
+--------------------------------------------------------------------------------------------------------------------------------------
+type Env = [(Int, String)]      -- (Environment) associative list. Maps group number to matched string
+type State = [(Env, String)]    -- state of all branches. Current environment and string to match
 
-type State = [(Int, String)] -- associative list
--- pattern, input string, list of all possible branches and inputs for them
-matchPatternExtra :: [PatternToken] -> Input -> [(State, Input)]
-matchPatternExtra _ [] = []                     -- no matches, return "empty state"
-matchPatternExtra [] input = [([], input)]
-matchPatternExtra (AnchorEnd:_)  [] = [([], [])]
-matchPatternExtra (AnchorEnd:_)  _  = [] 
-matchPatternExtra (Alteration numGroup alts:rest) input =
-    [(
-        (numGroup, matchedString) : nextState, nextInput
-     ) | alt <- alts,                                                           -- check all braches 
-         (stateAlt, inputAlt) <- matchPatternExtra alt input,                   -- get input for the recursive call
-         let matchedString  = take (length input - length matchedString) input, -- the only easy part
-         (nextState, nextInput) <- matchPatternExtra rest inputAlt              -- recursive call
+-- Extended version of the mainMatch function
+mainMatchExtra :: Pattern -> Input -> Bool
+mainMatchExtra rawPattern input
+    | (not . null) rawPattern && head rawPattern == '^' = any (null . snd) $ matchPatternExtra (tail pattern) [([], input)]
+    | otherwise = any (null . snd) $ matchPatternExtra pattern $ zip (repeat []) (tails input)
+    where pattern = (applyRepeat . parsePattern 1) rawPattern
+
+repeatMatchesExtra :: PatternToken -> Int -> Maybe Int -> Env -> String -> State
+repeatMatchesExtra _ _ _ _ [] = []
+-- Calling matchPatternExtra would yield State variable. We can't omit Env variables (e.g. "(a)+\\1" - we match some group within repetition)
+repeatMatchesExtra token minRep Nothing env input =
+     let state = matchPatternExtra testPattern [(env, input)] in -- to prevent infinite loops
+        case state of
+        [] -> []
+        _  -> state ++ (repeatMatchesExtra token (minRep+1) Nothing env input)
+    where testPattern = replicate minRep token
+repeatMatchesExtra token minRep (Just maxRep) env input 
+    | minRep <= maxRep = matchPatternExtra testPattern [(env, input)] ++ (repeatMatchesExtra token (minRep+1) (Just maxRep) env input)
+    | otherwise = []
+    where testPattern = replicate minRep token
+
+-- pattern, current state, list of all possible branches and inputs for them
+matchPatternExtra :: [PatternToken] -> State -> State
+-- Base case. Just return the accumulated state
+matchPatternExtra [] state = state
+-- For token types AnchorEnd, Literal, Meta, Group, Backreference just filter state and propagate it.
+matchPatternExtra (AnchorEnd:_) state  = filter (null . snd) state
+matchPatternExtra (Literal x:rest) state = matchPatternExtra rest nextState
+    where nextState = [(env, drop 1 input) | 
+            (env, input) <- state,
+            let recInput = tail input,
+            not (null input), x == (head input)
+            ]
+matchPatternExtra (Meta Digit:rest) state = matchPatternExtra rest nextState
+    where nextState = [(env, drop 1 input) | 
+            (env, input) <- state,
+            let nextInput = tail input,
+            not (null input), (isDigit . head) input
+            ]
+matchPatternExtra (Meta Word:rest) state = matchPatternExtra rest nextState
+    where nextState = [(env, drop 1 input) | 
+            (env, input) <- state,
+            let nextInput = tail input,
+            not (null input), (isWord . head) input 
+            ]
+matchPatternExtra (Group group neg:rest) state = matchPatternExtra rest nextState
+    where nextState = [(env, drop 1 input) | 
+            (env, input) <- state,
+            let nextInput = tail input,
+            not (null input), (inGroup . head) input 
+            ]
+          inGroup c = (c `elem` [x | Literal x <- group]) /= neg
+matchPatternExtra (Wildcard:rest) state = matchPatternExtra rest nextState
+    where nextState = [(env, drop 1 input) | (env, input) <- state, (not . null) input]
+matchPatternExtra (Backreference groupNum:rest) state = matchPatternExtra rest nextState
+    where nextState = [(env, nextInput) |
+            (env, input) <- state,
+            Just ref <- [lookup groupNum env],                                -- might return Nothing. Insterested only in Just values
+            let nextInput = drop (length ref) input,
+            ref `isPrefixOf` input
+            ]
+-- Add new inputs in state and go further
+matchPatternExtra (Repeater token minRep maxRep:rest) state = matchPatternExtra rest nextState
+    where nextState = foldl (++) [] [repeatMatchesExtra token minRep maxRep env input | (env, input) <- state]
+-- In case of Alternation, return final result. Note recursive calls within list comprehension
+matchPatternExtra (Alteration groupNum alts:rest) state =
+    [(newEnv, newInput) |
+        (env, input) <- state,                                              -- check all inputs
+        alt <- alts,                                                        -- check all alternatives
+        (_, nextInput) <- matchPatternExtra alt state,                      -- input for recursive call
+        let matchedString = take (length input - length nextInput) input,   -- string to update current environment
+        let currentMatch = (groupNum, matchedString),
+        let nextEnv = currentMatch : env,
+        let nextState = [(nextEnv, nextInput)],                             -- state for recursive call
+        (newEnv, newInput) <- matchPatternExtra rest nextState
     ]
-matchPatternExtra (Repeater token minRep maxRep:rest) input =
-    [ 
-    ]   
