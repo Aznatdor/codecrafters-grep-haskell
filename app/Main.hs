f5b4dae19bdf7e404c030e682ab0da7d22d8ae44
Aznatdor <abobrov333@gmail.com>
2025-10-15 13:01:31 +0300

    Refactor code

--- app/Main.hs
+++ app/Main.hs
@@ -1,27 +1,19 @@
 module Main where
 
-import System.Environment
-import System.Exit
-import System.IO (hPutStrLn, hSetBuffering, stdout, stderr, BufferMode (NoBuffering))
-import Grep.Matcher -- core logic of matching function
+import Grep.Handle
 
 main :: IO ()
 main = do
-  -- Disable output buffering
-  hSetBuffering stdout NoBuffering
-  hSetBuffering stderr NoBuffering
+    -- Disable output buffering
+    hSetBuffering stdout NoBuffering
+    hSetBuffering stderr NoBuffering
 
-  args <- getArgs
-  let pattern = args !! 1
-  input_line <- getLine
-
-  -- You can use print statements as follows for debugging, they'll be visible when running tests.
-  hPutStrLn stderr "Logs from your program will appear here"
-
-  if head args /= "-E"
-    then do
-      putStrLn "Expected first argument to be '-E'"
-      exitFailure
-    else do if mainMatchExtra pattern input_line
-              then exitSuccess
-              else exitFailure
+    args <- getArgs
+    case args of
+        ("-E":pattern:[]) -> handleLine pattern
+        ("-E":pattern:fileName:[]) -> handleFile pattern fileName
+        ("-E":pattern:fileNames) -> handleFiles pattern fileName
+        ("-r":"-E":pattern:dirName) -> handleDirRec pattern dirName
+        _ -> do
+            putStrLn "Invalid arguments" 
+            exitFailure
